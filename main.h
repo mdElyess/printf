b0399e85803d0f0261036c1910b012100ef2e582
Mohamed Ilyess Issaoui <mohamed@XDrive>
2023-07-03 15:17:02 +0100

    mandatory

--- main.h
+++ main.h
@@ -53,7 +53,7 @@ typedef struct converter_s
  */
 typedef struct flag_s
 {
-	unsigned char *flas;
+	unsigned char *flag;
 	unsigned int value;
 } flag_t;
 
@@ -92,8 +92,8 @@ unsigned char handle_flags(const char *flags, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *, unsigned char,
-		int, int, unsigned char);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
 
 /* Modifiers */
 unsigned int print_width(buffer_t *output, unsigned int printed,
